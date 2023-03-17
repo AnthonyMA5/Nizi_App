66bfb5d30d6639a9cff357e02b198bbbe85c2d69
Serenthony <116910131+Serenthony@users.noreply.github.com>
2023-03-17 01:38:30 -0600

    v1.0

--- src/screens/Menu.tsx
+++ src/screens/Menu.tsx
@@ -1,4 +1,5 @@
 /* eslint-disable prettier/prettier */
+/* eslint-disable react-native/no-inline-styles */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable eol-last */
 /* eslint-disable semi */
@@ -9,12 +10,14 @@ import { ScrollView } from 'react-native'
 import { Image } from 'react-native'
 import { TouchableOpacity } from 'react-native'
 import { TextInput } from 'react-native'
+import LinearGradient from 'react-native-linear-gradient'
 
 interface Props {
     navigation: DrawerNavigationProp<any, any>
 }
 
 const Menu: React.FC<Props> = ({navigation}) => {
+
   return (
     <SafeAreaView style={styles.main_container}>
       <ScrollView style={styles.scroll_container}>
@@ -55,7 +58,71 @@ const Menu: React.FC<Props> = ({navigation}) => {
           <Text style={styles.title}>Menú digital</Text>
           <Text style={styles.subtitle}>Platillos de la semana</Text>
 
-          
+          <View style={styles.sectionContainer}>
+              <ScrollView horizontal={true} style={{flex: 1}} showsHorizontalScrollIndicator={false}>
+
+                  <LinearGradient start={{x: 0, y: 0}} end={{x: 1, y: 0}} colors={['#5433FF', '#00E0FF']} style={styles.cardView}>
+
+                    <View style={styles.subcontainer_food}>
+                      <Image source={require('../img/platillo2.png')} style={styles.food_image}/>
+                    </View>
+
+                    <View style={styles.info_container}>
+                      <View style={styles.left_info}>
+                        <Text style={styles.text_name}>Ensalada de pollo con vegetales</Text>
+                      </View>
+
+                      <View style={styles.right_info}>
+                        <View style={styles.price_container}>
+                          <Text style={styles.text_price}>$ 85.00</Text>
+                        </View>
+                      </View>
+
+                    </View>
+                  </LinearGradient>
+
+                  <LinearGradient start={{x: 0, y: 0}} end={{x: 1, y: 0}} colors={['#CA33FF', '#0075FF']} style={styles.cardView}>
+
+                    <View style={styles.subcontainer_food}>
+                      <Image source={require('../img/platillo1.png')} style={styles.food_image}/>
+                    </View>
+
+                    <View style={styles.info_container}>
+                      <View style={styles.left_info}>
+                        <Text style={styles.text_name}>Ensalada de pollo con vegetales</Text>
+                      </View>
+
+                      <View style={styles.right_info}>
+                        <View style={styles.price_container}>
+                          <Text style={styles.text_price}>$ 85.00</Text>
+                        </View>
+                      </View>
+
+                    </View>
+                  </LinearGradient>
+
+                  <LinearGradient start={{x: 0, y: 0}} end={{x: 1, y: 0}} colors={['#5433FF', '#00E0FF']} style={styles.cardView}>
+
+                    <View style={styles.subcontainer_food}>
+                      <Image source={require('../img/platillo2.png')} style={styles.food_image}/>
+                    </View>
+
+                    <View style={styles.info_container}>
+                      <View style={styles.left_info}>
+                        <Text style={styles.text_name}>Ensalada de pollo con vegetales</Text>
+                      </View>
+
+                      <View style={styles.right_info}>
+                        <View style={styles.price_container}>
+                          <Text style={styles.text_price}>$ 85.00</Text>
+                        </View>
+                      </View>
+
+                    </View>
+                  </LinearGradient>
+
+              </ScrollView>
+          </View>
 
 
         </View>
@@ -153,7 +220,7 @@ const styles = StyleSheet.create({
     fontFamily: 'DMSans-Bold',
     fontSize: 24,
     color: '#000000',
-    marginTop: 35,
+    marginTop: 30,
   },
 
   subtitle:{
@@ -163,6 +230,69 @@ const styles = StyleSheet.create({
     marginTop: 25,
   },
 
+  sectionContainer:{
+    flex: 1,
+    marginTop: 25,
+    width: '100%',
+  },
+
+  cardView:{
+    width: 300,
+    height: 220,
+    borderRadius: 15,
+    marginRight: 12,
+  },
+
+  subcontainer_food:{
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 10,
+  },
+
+  food_image:{
+    width: 130,
+    height: 130,
+  },
+
+  info_container:{
+    flexDirection: 'row',
+    marginTop: 15,
+    marginStart: 15,
+    marginEnd: 15,
+  },
+
+  left_info:{
+    flex: 0.7,
+    justifyContent: 'center',
+    alignItems: 'flex-start',
+  },
+
+  text_name:{
+    fontFamily: 'DMSans-Medium',
+    fontSize: 15,
+    color: '#FFF',
+  },
+
+  right_info:{
+    flex: 0.3,
+    justifyContent: 'center',
+    alignItems: 'flex-end',
+  },
+
+  price_container:{
+    padding: 8,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 20,
+    backgroundColor: '#000',
+  },
+
+  text_price:{
+    fontFamily: 'DMSans-Medium',
+    fontSize: 14,
+    color: '#FFF',
+  },
 
 
 })
